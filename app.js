cb94d5c6be0ea04be2e3a2130caf08004a4eee2a
Riccardo Manzato <riccardo.manzato@peri-polytech.com>
2021-01-04 15:02:51 +0100

    Aggiunta cors a localhost

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 //var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
+var cors = require('cors')
+
+app.use(cors({
+    origin: 'http://localhost:3000/'
+})) 
 app.use(express.json())
 //app.use(cors())
 
